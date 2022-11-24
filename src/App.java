e63a2811d5181b08157562cdfc84edea0024e5ed
Juan Paulo Labrin <juanlabrin@gmail.com>
2022-11-24 20:28:49 -0300

    Changes in App.java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Jenkins!");
-        int n1 = 5;
-        int n2 = 8;
-        System.out.println(n1*n2);
         System.out.println("Jenkins Integration is Done");
     }
 }
