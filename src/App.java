930840679fc0ce9c10bc027e1acd716fc2c73043
Juan Paulo Labrin <juanlabrin@gmail.com>
2022-11-24 18:14:24 -0300

    First change

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Jenkins!");
+        int n1 = 5;
+        int n2 = 8;
+        System.out.println(n1*n2);
     }
 }
